826e9e6bb6a75b5c6cdb3ba5700d822471003772
developer <>
2026-02-04 16:33:29 -0600

    Creación de Estructura MVVM carpedato

--- app/src/main/java/com/linkersconsulting/activegym/main/MainActivity.kt
+++ app/src/main/java/com/linkersconsulting/activegym/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.linkersconsulting.activegym
+package com.linkersconsulting.activegym.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.linkersconsulting.activegym.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
