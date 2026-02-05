291c6ea92dd3d5c7f56b13b1f456e1560669b5ad
developer <>
2026-02-05 11:53:44 -0600

    Se implementa material desing y se borran elementos basura de los fragmentos creados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+//Material desing
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
+
+
+
 }
