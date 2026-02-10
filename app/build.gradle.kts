b8e9a70956993405b896a7fa5a82b96d958131e9
Luis Mario Vanegas Cruz <balon39@hotmail.com>
2026-02-09 23:10:21 -0600

    Se implemento la parte de FIREBASE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -56,6 +57,10 @@ dependencies {
     // Navigation para findNavController
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    //FIREBASE
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
 
 
 }
